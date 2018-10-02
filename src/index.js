15b462cc3002da7d6b0395b9a2b4d49753a244ab
Alexandr Shushunov <ashushunov@mail.ru>
2018-10-03 00:19:19 +0300

    Добавлена перегрузка

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 // @flow
-const wordCount = (str: string) =>
-  str.split(' ').length;
+const wordCount = (
+  str: string | Array<string>
+) => {
+  if (Array.isArray(str)) {
+    return str.reduce(
+      (prev, curr) =>
+        prev + curr.split(' ').length,
+      0
+    );
+  }
+  return wordCount([str]);
+};
 
-const wordCountInArray = (str: Array<string>) =>
-  str.reduce(
-    (prev, curr) => curr.split(' ').length,
-    0
-  );
-
-console.log(wordCount('Hi world'));
+console.log(wordCount(['Hi world', 'one more']));
