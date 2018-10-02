73d8772d56c67c7802f52be0e671f504432a5c9e
Alexandr Shushunov <ashushunov@mail.ru>
2018-10-03 00:15:41 +0300

    Добавлена функция подсчета слов в массиве

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@
 const wordCount = (str: string) =>
   str.split(' ').length;
 
+const wordCountInArray = (str: Array<string>) =>
+  str.reduce(
+    (prev, curr) => curr.split(' ').length,
+    0
+  );
+
 console.log(wordCount('Hi world'));
