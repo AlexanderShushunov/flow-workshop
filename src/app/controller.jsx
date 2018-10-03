3d5071a9331c3d528c8ca15c8025eda8186dac7c
Alexandr Shushunov <ashushunov@mail.ru>
2018-10-04 01:30:03 +0300

    Протипизирован контроллер

--- src/app/controller.jsx
+++ src/app/controller.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+// @flow
+import * as React from 'react';
 import {isNormalCell, TicTacToe} from '../game';
 import {Layout} from './layout';
+import type {Snapshot} from '../game';
 
-export class Controller extends React.Component {
+export class Controller extends React.Component<{||}, { snapshot: Snapshot }> {
 
     state = {
         snapshot: TicTacToe.initial()
     };
 
-    selectCell = (row, column) => this.setState(
+    #selectCell = (row, column) => this.setState(
         ({snapshot}) => ({
             snapshot: TicTacToe.next(snapshot, row, column)
         })
@@ -18,14 +20,14 @@ export class Controller extends React.Component {
         snapshot: TicTacToe.initial()
     });
 
-    isWinCell = (row, column) => !isNormalCell(row, column);
+    #isWinCell = (row, column) => !isNormalCell(this.state.snapshot)(row, column);
 
     render() {
         return (<Layout
             onResetClick={this.resetGame}
             snapshot={this.state.snapshot}
-            isWinCell={this.isWinCell}
-            onCellClick={this.selectCell}
+            isWinCell={this.#isWinCell}
+            onCellClick={this.#selectCell}
             canReset={this.state.snapshot.result !== 'turn'}
         />);
     }
